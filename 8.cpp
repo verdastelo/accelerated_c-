73f567c8993c1ffa68cba9f58a0c176ea6b86026
verdastelo <verda_stelo@hotmail.com>
2015-11-21 07:18:12 +0530

    Introductory chapter "Getting Started" of Accelerated C++.

---
+++ 8.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+int main() 
+{
+    // This is a comment that extends over several lines
+    //because it uses // at the beginning of each line instead of using /*
+    // or */ to delimit comments. 
+    
+    std::cout << "Does this work?" << std::endl; 
+    
+    return 0; 
+}
---
+++ 9.cpp
@@ -0,0 +1,5 @@
+// Mi pensas, ke la suba kodo estas la
+// plej mallonga programo en C++. 
+
+int main() {
+    }
